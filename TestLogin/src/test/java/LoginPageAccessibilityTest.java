2807d9a947dff39cdea4fb5941b6aad1b69dd8bd
bidushiKabir <140823643+bidushiKabir@users.noreply.github.com>
2024-09-23 02:47:57 -0400

    feat : add new positive test

--- TestLogin/src/test/java/LoginPageAccessibilityTest.java
+++ TestLogin/src/test/java/LoginPageAccessibilityTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -13,7 +14,9 @@ public class LoginPageAccessibilityTest {
 
     @BeforeEach
     public void starter() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
         url = "http://localhost:8080/";
     }
 
